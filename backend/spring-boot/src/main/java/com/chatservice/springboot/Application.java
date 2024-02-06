e11ca7402d5caf3ba08500434011059179833e12
Matias Frimodig <matias.frimodig@tuni.fi>
2024-02-06 23:22:14 +0200

    Added authentication with OAuth2.0 (google), added paths to home, login, and greet, also a callback path for login. Also modified the structure a bit

--- backend/spring-boot/src/main/java/com/chatservice/springboot/Application.java
+++ backend/spring-boot/src/main/java/com/chatservice/springboot/Application.java
@@ -3,11 +3,25 @@ package com.chatservice.springboot;
 import org.springframework.boot.SpringApplication;
    import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-   @SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // Remove the exclude when adding a database
-   public class SpringBootChatApplication {
+import java.util.Collections;
+import java.util.Map;
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // Remove the exclude when adding a database
+   @RestController
+   public class Application {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
 
        public static void main(String[] args) {
-           SpringApplication.run(SpringBootChatApplication.class, args);
+           SpringApplication.run(Application.class, args);
        }
    }
